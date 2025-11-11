bd137c581f5ff68c0240478a440711aaf7001baf
Feysel Semeredin <gundantech@gmail.com>
2025-11-11 10:21:16 +0300

    enhanced user input variable naming

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-current_weather = input ("What's the weather like today? (sunny/rainy/cold): ").lower()
+user_input = input ("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-if current_weather == "sunny":
+if user_input == "sunny":
     print ("Wear a t-shirt and sunglasses.")
-elif current_weather == "rainy":
+elif user_input == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif current_weather == "cold":
+elif user_input == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
