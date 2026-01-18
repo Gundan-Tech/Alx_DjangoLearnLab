5ad81acd9c574b857b9b664b8d93886957bb494c
Feysel Semeredin <gundantech@gmail.com>
2026-01-18 23:33:54 +0300

    Fix views imports, urls, and templates for relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
@@ -7,6 +7,7 @@ from .models import Library
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+
 
 class LibraryDetailView(DetailView):
     model = Library
