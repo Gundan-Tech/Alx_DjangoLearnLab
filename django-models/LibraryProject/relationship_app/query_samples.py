0e385a3c5f29c702c0b09b5161b430bb81d191a5
Feysel Semeredin <gundantech@gmail.com>
2026-01-18 23:05:48 +0300

    Fix query_samples to match checker requirements

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,14 +2,15 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 author = Author.objects.get(name="George Orwell")
-books_by_author = Book.objects.filter(author=author)
-print(books_by_author)
+books = Book.objects.filter(author=author)
+print(books)
 
 # List all books in a library
-library = Library.objects.get(name="Central Library")
-books_in_library = library.books.all()
-print(books_in_library)
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+library_books = library.books.all()
+print(library_books)
 
 # Retrieve the librarian for a library
-librarian = library.librarian
+librarian = Librarian.objects.get(library=library)
 print(librarian)
