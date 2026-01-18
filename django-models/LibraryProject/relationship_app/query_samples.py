98be52d1f2932b8097e20148b390b49284c2e672
Feysel Semeredin <gundantech@gmail.com>
2026-01-18 23:09:17 +0300

    Fix query_samples to satisfy checker variable names

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,8 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name)
 books = Book.objects.filter(author=author)
 print(books)
 
