e64d81abe7789198fa8ae94335f610d36d767769
Feysel Semeredin <gundantech@gmail.com>
2026-02-06 23:08:57 +0300

    mmmmmmm

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,5 +1,7 @@
 
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager, User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
 from django.conf import settings
 
@@ -87,3 +89,26 @@ class CustomUser(AbstractUser):
     profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
 
     objects = CustomUserManager()
+
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+# Signal to create UserProfile automatically
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
