49c00e156e0e267ec93b2cfe63952ba926372b5d
Feysel Semeredin <gundantech@gmail.com>
2026-02-06 23:24:05 +0300

    added a role based access control code

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Book
 from .models import Library
-
+from django.contrib.auth.decorators import user_passes_test
 
 def list_books(request):
     books = Book.objects.all()
@@ -30,4 +30,26 @@ def register(request):
     return render(request, 'relationship_app/register.html', {'form': form})
 
 
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
 
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
