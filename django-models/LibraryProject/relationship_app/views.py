f44bfa6d38f227ba3d68690dd6fda79065d0c715
Feysel Semeredin <gundantech@gmail.com>
2026-02-06 23:33:32 +0300

    enhancment

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Book
 from .models import Library
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required, user_passes_test
 
 def list_books(request):
     books = Book.objects.all()
