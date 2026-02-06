4775aac913bc7e84c225d963574c842466aa5ebc
Feysel Semeredin <gundantech@gmail.com>
2026-02-06 22:22:56 +0300

    enhanced the failed checker

--- advanced_features_and_security/LibraryProject/relationship_app/urls.py
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -5,8 +5,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    
-    # Use single quotes for template_name as the checker is very picky
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
