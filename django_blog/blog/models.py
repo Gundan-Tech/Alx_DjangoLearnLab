10523fba278dce3b4e8c26271a665181bf54e01f
Feysel Semeredin <gundantech@gmail.com>
2026-02-15 03:40:16 +0300

    added a post management code

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -7,5 +8,8 @@ class Post(models.Model):
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+    
     def __str__(self):
         return self.title
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,15 @@ from .models import Post
 from .forms import PostForm
 from .forms import SignUpForm, UserUpdateForm
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView
+)
+from .models import Post
 
 
 def register(request):
@@ -84,3 +93,45 @@ def post_delete(request, pk):
 
 def home(self):
     return render(self, 'blog/base.html')
+
+# 1. ListView - Displays all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # Expected: <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+# 2. DetailView - Displays a single post
+class PostDetailView(DetailView):
+    model = Post
+
+# 3. CreateView - Authenticated users can create posts
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Set current user as author
+        return super().form_valid(form)
+
+# 4. UpdateView - Only the author can edit
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Permission check
+
+# 5. DeleteView - Only the author can delete
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/posts/'  # Redirect here after deletion
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
